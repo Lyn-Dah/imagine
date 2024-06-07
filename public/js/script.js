797fcd0fbd65de4f135ada151c03e606a2c48646
Linda Edu <edulind99@gmail.com>
2024-06-07 10:56:08 +0000

    Strings in JavaScript

--- public/js/script.js
+++ public/js/script.js
@@ -117,3 +117,60 @@ const tweet1 = {
   
   
   
+  // Functions
+// Defining functions
+function login(username, password) {
+  if(username== 'lynny'&& password== '1234') {
+   return "User is logged in!"; 
+  } else if (username != 'lynny') {
+    return 'Invalid username!';
+  } else if (password != '1234') {
+    return 'Invalid password!';
+  } else { 
+    return 'Invalid username or password'
+  }
+ }
+
+// Invoking functions
+login('lynny', '1234');
+
+
+// Basic Arithmetic
+12 + 34;
+45 - 16;
+7 * 12;
+78 / 4;
+78 % 4;
+
+// Comparison operators
+2 == '2';
+3 !== 2;
+
+//  Built-in Math functions
+Math.round(37 / 24);
+Math.floor(37 / 24);
+Math.ceil(37 / 24);
+Math.random();
+Math.max(78, 12, 98, 14, 89);
+
+// string concatenation
+const firstname = 'Linda ';
+const lastname = 'Edu';
+firstname.trim() + ' ' + lastname.trim();
+
+// Template literal
+`${firstname.trim()} ${lastname.trim()}`;
+
+// String methods
+firstname.length;
+lastname.length;
+`${firstname.trim()} ${lastname.trim()}`.length;
+firstname.toUpperCase();
+lastname.charAt(0);
+`${firstname.trim()} ${lastname.trim()}`.charAt(0);
+`${firstname.trim().charAt(0)}${lastname.trim().charAt(0)}`;
+
+// String conversion
+String(2014);
+
+
